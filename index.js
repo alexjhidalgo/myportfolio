ceeb321ac7edbc93a7086f8d542bb2a65124bbcf
aalexmann <alex.h@Alexs-MacBook.local>
2019-10-21 05:44:07 -0400

    added photos, media queries, and css

---
+++ index.js
@@ -0,0 +1,10 @@
+function tellMeMore(){
+    $(".shown").on('click', 'h2', function(){
+        $(".shown").toggleclass(".hidden");
+    })
+}
+function canYouDont(){
+    $(document).ready(function(){
+        $('shown').toggleclass('hidden')
+    })
+}
