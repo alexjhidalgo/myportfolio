6c8dc9530831475247922c7fd3139e16a369e32a
aalexmann <55524595+aalexmann@users.noreply.github.com>
2021-02-09 01:58:45 -0800

    made project and contact pages in construction mode. deployable.

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ function myFunction() {
       x.style.display = "block";
     }
   };
+
+function goToAboutMePage() {
+    var x = document.getElementById("about-me-page");
+    if (x.style.display === "block"){
+        x.style.display = "block"
+    }
+    else {
+        document.getElementById("projects-page").style.display = "none";
+        document.getElementById("contact-page").style.display = "none";
+        x.style.display = "block"
+    }
+}
+
+function goToProjectsPage() {
+    var x = document.getElementById("projects-page");
+    if (x.style.display === "block"){
+        x.style.display = "block"
+    }
+    else {
+        document.getElementById("about-me-page").style.display = "none";
+        document.getElementById("contact-page").style.display = "none";
+        x.style.display = "block"
+    }
+}
+
+function goToContactPage() {
+    var x = document.getElementById("contact-page");
+    if (x.style.display === "block"){
+        x.style.display = "block"
+    }
+    else {
+        document.getElementById("projects-page").style.display = "none";
+        document.getElementById("about-me-page").style.display = "none";
+        x.style.display = "block"
+    }
+}
