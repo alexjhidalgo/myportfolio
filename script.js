d7cd6f1808b07b50384154e5070d303f636c8e92
aalexmann <55524595+aalexmann@users.noreply.github.com>
2021-12-16 16:39:20 -0800

    new look, no image carousel

--- script.js
+++ script.js
@@ -8,22 +8,30 @@ function myFunction() {
     }
   };
 
-function goToMePage() {
+function goToMePage()
+{
+    // Gets url and removes file name from endpoint and updates it to have me.html 
+    // as the page.
     let path = window.location.pathname;
     let page = path.split("/").pop();
     let kay = path.length - page.length;
     window.location.href = `${path.slice(0,kay)}me.html`;
-
 }
 
-function goToProjectsPage() {
+function goToProjectsPage()
+{
+    // Gets url and removes file name from endpoint and updates it to have my_work.html 
+    // as the page.
     let path = window.location.pathname;
     let page = path.split("/").pop();
     let kay = path.length - page.length;
     window.location.href = `${path.slice(0,kay)}my_work.html`;
 }
 
-function goToContactPage() {
+function goToContactPage()
+{
+    // Gets url and removes file name from endpoint and updates it to have contact.html 
+    // as the page.
     let path = window.location.pathname;
     let page = path.split("/").pop();
     let kay = path.length - page.length;
