e20ecd0578f64885d5817c1b24164ae6754a6261
Alex Hidalgo <alexjhidalgo@icloud.com>
2024-02-02 05:02:46 -0500

    Added some css for layout and newspaper color, new fonts too.

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -4,10 +4,9 @@ import '../styles/Header.css';
 const Header = () => {
     return (
         <header className="header">
-            <div className="ornamental-graphic left">Home Grown/Organic JavaScript and React!</div>
+            <div className="ornamental-graphic-left">Home Grown/Organic <br></br>JavaScript and React!</div>
             <div className="center-graphic"><h1>Hidalgo Post</h1></div>
-            <div className="ornamental-graphic right">Built From Scratch by Alex Hidalgo!</div>
-            <div className="ornamental-graphic right-2">Truly a Vision for AJH!</div>
+            <div className="ornamental-graphic-right">Built From Scratch by Alex Hidalgo!<br></br>A True Vision from AJH!</div>
         </header>
     );
 };
--- react-portfolio/src/pages/HomePage.js
+++ react-portfolio/src/pages/HomePage.js
@@ -6,134 +6,146 @@ import '../styles/HomePage.css';
 
 const HomePage = () => {
 
+    const currentDate = new Date();
+    const dayOfWeek = currentDate.toLocaleString('en-US', { weekday: 'long' });
+    const month = currentDate.toLocaleString('en-US', { month: 'long' });
+    const dayOfMonth = currentDate.getDate();
+    const year = currentDate.getFullYear();
+
     return (
-        <div>
+        <div className='homepage'>
             <Header />
-            
-            <div className="personal-column-left-narrow">
-
-                <Tile 
-                    title="History" 
-                    tileClassName="tile"
-                    imgClassName="tile-img"
-                    description="La Quinta, CA, a Charmed Life" 
-                    image="/date-palm-inner-with-van.png"/>
-
-                <Tile 
-                    title="Family" 
-                    tileClassName="tile"
-                    imgClassName="tile-img"
-                    description="Hidalgo, Hays, and Valdivia, legacies of entrepreneurship." 
-                    image="/kyrie-beach-stairs.png"/>
-            </div>
-
-            <div className="main-column-top">
-
-                <div className="main-column-left">
-                    <Tile 
-                        title="Handwritten" 
-                        tileClassName="tile"
-                        imgClassName="tile-img"
-                        description="iOS App using text recognition to digitize your handwriting to send personal letters." 
-                        image="you-are-beautiful-writing.gif"/>
+            <div className="date">{dayOfWeek}, {month} {dayOfMonth}, {year}</div>
+            <h2>Bytes of Alex, for</h2>
+            <div className="columns">
+                
+                <div className="personal-column-left-narrow">
 
                     <Tile 
-                        title="Doge Farms" 
+                        title="History" 
                         tileClassName="tile"
                         imgClassName="tile-img"
-                        description="Connects dog breeders (seasoned or aspiring) with loving owners." 
-                        image="doge-words.gif"/>
+                        description="La Quinta, CA, a Charmed Life" 
+                        image="/date-palm-inner-with-van.png"/>
 
                     <Tile 
-                        title="Hidalgo Properties" 
+                        title="Family" 
                         tileClassName="tile"
                         imgClassName="tile-img"
-                        description="Business Website for Property Management, Listings, and Leads on New Properties." 
-                        image="property-mgmt.png"/>
-
-                    <Tile 
-                        title="Shelter Space" 
-                        tileClassName="tile"
-                        imgClassName="tile-img"
-                        description="iOS App for finding available shelter space for the unhoused." 
-                        image="homeless-shelter.png"/>
+                        description="Hidalgo, Hays, and Valdivia, legacies of entrepreneurship." 
+                        image="/kyrie-beach-stairs.png"/>
                 </div>
 
-                <div className="main-column-right">
-                    <Tile 
-                        title="Big Farma" 
-                        tileClassName="tile"
-                        imgClassName="tile-img"
-                        description="Example Description" 
-                        image="pompeii-vineyard.png"/>
+                <div className="main-column-top">
+                    
+                    <div className="main-column-left">
+                            <h3>The Left</h3>
+                            <Tile 
+                                title="Handwritten" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="iOS App using text recognition to digitize your handwriting to send personal letters." 
+                                image="you-are-beautiful-writing.gif"/>
 
-                    <Tile 
-                        title="Travel Perspective" 
-                        tileClassName="tile"
-                        imgClassName="tile-img"
-                        description="Historically never been easier" 
-                        image="wing-over-water.png"/>
+                            <Tile 
+                                title="Doge Farms" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="Connects dog breeders (seasoned or aspiring) with loving owners." 
+                                image="doge-words.gif"/>
 
-                    <Tile 
-                        title="California Real Estate" 
-                        tileClassName="tile"
-                        imgClassName="tile-img"
-                        description="Why the price jumps kinda make sense" 
-                        image="old-oakland.png"/>
+                            <Tile 
+                                title="Hidalgo Properties" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="Business Website for Property Management, Listings, and Leads on New Properties." 
+                                image="property-mgmt.png"/>
 
-                    <Tile 
-                        title="Know Thyself, Relish Each Step" 
-                        tileClassName="tile"
-                        imgClassName="tile-img"
-                        description="A persuasive essay about writing habitually" 
-                        image="journal-ios.png"/>
+                            <Tile 
+                                title="Shelter Space" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="iOS App for finding available shelter space for the unhoused." 
+                                image="homeless-shelter.png"/>
+                        </div>
 
-                    <Tile 
-                        title="Healthcare.. I've Got It?" 
-                        tileClassName="tile"
-                        imgClassName="tile-img"
-                        description="It's not perfect, yet still the best." 
-                        image="medicaid.png"/>
+                        <div className="main-column-right">
+                            <h3>The Right</h3>
+                            <Tile 
+                                title="Big Farma" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="One day I shall return to my farm like old Cinncinatus." 
+                                image="pompeii-vineyard.png"/>
+
+                            <Tile 
+                                title="Travel Perspective" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="Historically never been easier" 
+                                image="wing-over-water.png"/>
+
+                            <Tile 
+                                title="California Real Estate" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="Why the price jumps kinda make sense" 
+                                image="old-oakland.png"/>
+
+                            <Tile 
+                                title="Know Thyself, Relish Each Step" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="A persuasive essay about writing habitually" 
+                                image="journal-ios.png"/>
+
+                            <Tile 
+                                title="Healthcare.. I've Got It?" 
+                                tileClassName="tile-main"
+                                imgClassName="tile-img-main" 
+                                description="It's not perfect, yet still the best." 
+                                image="medicaid.png"/>
+                        </div>
 
                 </div>
 
-            </div>
+                <div className="business-column-right-narrow">
+                    <Tile 
+                        title="Travel" 
+                        tileClassName="tile"
+                        imgClassName="tile-img"
+                        description="Did you read that opinion piece? or puff piece? .. either way, get on my level! (i'll help you)" 
+                        image="aina-haina-deck.png"/>
 
-            <div className="business-column-right-narrow">
-                <Tile 
-                    title="Travel" 
-                    tileClassName="tile"
-                    imgClassName="tile-img"
-                    description="Did you read that opinion piece? or puff piece? .. either way, get on my level! (i'll help you)" 
-                    image="aina-haina-deck.png"/>
+                    <Tile 
+                        title="Legal Services" 
+                        tileClassName="tile"
+                        imgClassName="tile-img"
+                        description="Don't get taken for a ride. We'll also write your will with you." 
+                        image="justice-for-all.png"/>
 
-                <Tile 
-                    title="Legal Services" 
-                    tileClassName="tile"
-                    imgClassName="tile-img"
-                    description="Don't get taken for a ride. We'll also write your will with you." 
-                    image="justice-for-all.png"/>
+                    <Tile 
+                        title="Real Estate Services (License Pending)" 
+                        tileClassName="tile"
+                        imgClassName="tile-img"
+                        description="I can help; im kind of obsessed." 
+                        image="real-estate.png"/>
 
-                <Tile 
-                    title="Real Estate Services (License Pending)" 
-                    tileClassName="tile"
-                    imgClassName="tile-img"
-                    description="I can help; im kind of obsessed." 
-                    image="real-estate.png"/>
+                    <Tile 
+                        title="Financial Planning" 
+                        tileClassName="tile"
+                        imgClassName="tile-img"
+                        description="I see the opportunities for surplus in your future!" 
+                        image="finances.png"/>
 
-                <Tile 
-                    title="Financial Planning" 
-                    tileClassName="tile"
-                    imgClassName="tile-img"
-                    description="I see the opportunities for surplus in your future!" 
-                    image="finances.png"/>
-
-                <Tile 
-                    title="Education Consultation" 
-                    tileClassName="tile"
-                    imgClassName="tile-img"
-                    description="Let's get you learned." 
-                    image="education-access.png"/>
+                    <Tile 
+                        title="Education Consultation" 
+                        tileClassName="tile"
+                        imgClassName="tile-img"
+                        description="Let's get you learned." 
+                        image="education-access.png"/>
+                </div>
+                
             </div>
             <Footer />
         </div>
